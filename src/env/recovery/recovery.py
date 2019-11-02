173458f5f631b5eb7b16fb835ea2ae2b908fba1a
OwenDurkin <owen@thedurks.com>
2019-11-02 18:52:41 -0400

    add logic to step function: drone networks update as a group

--- src/env/recovery/recovery.py
+++ src/env/recovery/recovery.py
@@ -3,6 +3,7 @@ from gym import error, spaces, utils
 from gym.utils import seeding
 
 import numpy as np
+from functools import reduce
 
 from ..Drone import Drone
 
@@ -42,8 +43,72 @@ class Recovery(gym.Env):
         for i in range(cluster_count):
             pass
 
+
+    '''
+    helper functions for step function
+    '''
+    def get_bin(self, d):
+        return (int(d.loc[0]//VISION), int(d.loc[1]//VISION))
+
+    # drones are connected if they have a "path" of connection to one another
+    # this function returns a list of drone isolated networks
+    def get_connected_components(self):
+        # set up data structure to handle "collisions" of drones
+        bins = [[set() for y in range(int(HEIGHT//VISION))] for x in range(int(WIDTH//VISION))]
+        # place drones in their respective bins
+        for d in drones:
+            x,y = self.get_bin(d)
+            bins[x][y].add(d)
+        
+        # DFS drones to get connected components
+        components = []
+        unexplored = set(self.drones)
+        while unexplored.size() > 0:
+            # find the component of a random unexplored drone
+            queue = [unexplored.pop()]
+            component = set()
+            while queue.size() != 0:
+                cur = queue.pop()
+                # if we haven't seen cur before, add it to this component,
+                # as well as its unexplored neighbors
+                if cur in unexplored:
+                    component.add(cur)
+                    # find unexplored neighbors and add them to the queue
+                    # only check this and surrounding bins
+                    for i in range(-1,2):
+                        for j in range(-1,2):
+                            x, y = self.get_bin(cur)
+                            x += i
+                            y += j
+                            if x < 0 or x >= bins.length()  or y < 0 or y >= bins[0].length(): continue
+                            # check all drones in this bin
+                            bin = bins[x][y]
+                            for d in bin:
+                                if cur.is_in_range(d) and d in unexplored:
+                                    queue.append(d)
+                                    unexplored.remove(d)
+            # completed component: add to the list of components
+            components.append(component)
+        return components
+    
+    # step function: runs every iteration of the simulation
     def step(self, action):
-        pass
+        # determine the isolated networks
+        components = self.get_connected_components()
+        
+        # consolidate information in each connected components
+        for component in components:
+            net_people_locs = reduce((lambda x,y: x.union(y)), [set()]+[d.get_people() for d in component])
+            net_explored_locs = reduce((lambda x,y: x.union(y)), [set()]+[d.get_explored_locs() for d in component])
+            for drone in component:
+                drone.set_people_locs(net_people_locs)
+                drone.set_explored_locs(net_explored_locs)
+        
+        # tell all of the drones to move
+        for d in self.drones:
+            d.calc_move()
+        for d in self.drones:
+            d.move()
 
     def reset(self):
         pass

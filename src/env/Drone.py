2d5d35250533f633bbd76741c4df9b15c74a8b85
OwenDurkin <owen@thedurks.com>
2019-11-02 17:22:32 -0400

    update Drone.py

--- src/env/Drone.py
+++ src/env/Drone.py
@@ -1,59 +1,51 @@
-import spaces
 from gym import spaces
+import sys
+
+DRONE_SPEED = 1.0
 
 class Drone:
     def __init__(self, base_camp_loc):
         self.base_camp_loc = base_camp_loc
         self.last_seen_drone = base_camp_loc        
-        self.loc = base_camp_loc
-        
+        self.loc = base_camp_loc # + random noise???
+
         self.people_locs = set() # floating points
         self.explored_locs = set() # tiles in grid
-        
-        # up down left right : 0 1 2 3
-        self.action_space = spaces.Discrete(4)
-        
+
     '''
     @param vision: the set of people/locations that this drone can see
     '''
     def look(self, vision):
         self.people_locs = self.people_locs.union(vision)
-    
+
     '''
     return a grid representation of the information collected so far
     '''
     def message(self):
-        return self.people_locs
-    
-    def receive(self, ppl):
+        return self.people_locs, explored_locs
+
+    '''
+    receive an update from the connected network
+    '''
+    def receive(self, people_locs, explored_locs):
         look(ppl)
-       
-    '''
-    @param observation is a N x N grid representing the tiles that this drone can see
-    @param reward ML?
-    @param done for when he simulation is over
-    '''
-    def act(self, observation, reward, done):
+        self.explored_locs = self.explored_locs.union(explored_locs)
+
+
+    def move(self):
+        # use ML algorithm to determine direction
         
-        # record visible people
+        # each between -1 and 1
+        dir_x = 0.0
+        dir_y = 0.0
         
+        dir_len = (dir_x**2+dir_y**2)**(0.5)
         
-        # TODO use ML to determine action
-        action = self.action_space.sample()
-        
-        # up down left right
-        if action == 0:
-            self.loc[1] += 1
-        elif action == 1:
-            self.loc[1] -= 1
-        elif action == 2:
-            self.loc[0] -= 1
-        elif action == 3:
-            self.loc[0] += 1
-        
-        return action
-    
-    
+        # actual difference in location
+        if dir_len > sys.float_info.epsilon:
+            self.loc[0] += dir_x/dir_len
+            self.loc[1] += dir_y/dir_len
+            
 
         
 if __name__ == '__main__':
